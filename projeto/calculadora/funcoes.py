a32482e476cc5983ab3fde866b53a342c4c7d5ba
Marcio Magrini <marciomagrini@gmail.com>
2023-03-29 16:06:27 -0300

    criada def multiplicacao

--- projeto/calculadora/funcoes.py
+++ projeto/calculadora/funcoes.py
@@ -32,3 +32,13 @@ def divisao(a, b):
     except:
         div = 'erro desconhecido'
         print(f'1/{b} = {div}')
+
+def multiplicacao(a, b):
+    try:
+        test_a = a + 1
+        test_b = b + 1
+    except TypeError:
+        print(
+            f"Os inputs A e B devem ser int ou float. Recebemos {a}: {type(a)}, e {b}: {type(b)}")
+    else:
+        return a * b

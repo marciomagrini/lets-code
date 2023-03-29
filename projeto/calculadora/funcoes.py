81ffbe842dd0856cbbaa7fb7d23e83f5dfa9dcc0
Marcio Magrini <marciomagrini@gmail.com>
2023-03-29 15:44:14 -0300

    atualizando def soma

--- projeto/calculadora/funcoes.py
+++ projeto/calculadora/funcoes.py
@@ -1,10 +1,14 @@
 def soma(a, b):
     try:
-        return a + b
+        test_a = a + 1
+        test_b = b + 1
     except TypeError:
         print(
             f"Os inputs A e B devem ser int ou float. Recebemos {a}: {type(a)}, e {b}: {type(b)}")
+    else:
+        return a + b
 
 
 def subtracao(a, b):
     return a - b
+

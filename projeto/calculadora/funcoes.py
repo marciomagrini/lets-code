26ba98b522cb0d8bf8fcb9d243c3c465a43aa753
Marcio Magrini <marciomagrini@gmail.com>
2023-03-29 16:01:40 -0300

    criada def divisao

--- projeto/calculadora/funcoes.py
+++ projeto/calculadora/funcoes.py
@@ -18,3 +18,17 @@ def subtracao(a, b):
             f"Os inputs A e B devem ser int ou float. Recebemos {a}: {type(a)}, e {b}: {type(b)}")
     else:
         return a - b
+
+
+def divisao(a, b):
+    try:
+        return a/b
+    except ZeroDivisionError:
+        div = 'infinito'
+        print(f'1/{b} = {div}')
+    except TypeError:
+        div = f'1/{b}'
+        print(f'1/{b} = {div}')
+    except:
+        div = 'erro desconhecido'
+        print(f'1/{b} = {div}')
